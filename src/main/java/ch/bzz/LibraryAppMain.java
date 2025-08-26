633db50488f916094a3635a10c07f583430f7217
im23b-schneidert <theodorstud@gmail.com>
2025-08-26 13:28:12 +0200

    implemented help and quit function

--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -1,8 +1,28 @@
 package ch.bzz;
 
+
+import java.util.Scanner;
+
 public class LibraryAppMain {
 
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        System.out.println("Welcome to LibraryApp! Type 'help' for commands.");
+        while (true) {
+            System.out.print("> ");
+            input = scanner.nextLine().trim().toLowerCase();
+            if (input.equals("quit")) {
+                System.out.println("Exiting");
+                break;
+            } else if (input.equals("help")) {
+                System.out.println("commands:");
+                System.out.println("- help");
+                System.out.println("- quit");
+            } else {
+                System.out.println("Unknown command: '" + input + "'. Type 'help' for a list of commands.");
+            }
+        }
+        scanner.close();
     }
 }
